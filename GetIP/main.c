3d802ce146d02a888bc3da4948736a914f2e0e1a
QGB <qgbcs1@gmail.com>
2017-10-28 18:50:18 +0800

    reg_wshell

---
+++ GetIP/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windef.h>
+//#include <iphlpapi.h>
+//DWORD WINAPI GetIpAddrTable(PMIB_IPADDRTABLE,PULONG,BOOL);
+#include "common.h"
+#include "sysinfo.h"
+int main()
+{
+    printf("Hello %i world!\n",77);
+    return 0;
+}
